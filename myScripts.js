7bf93acd98cece990384c3bf98d9708e2ddb56a4
Mirkomilboy <35518234+Mirkomilboy@users.noreply.github.com>
2018-09-25 19:26:29 +0500

    Add files via upload

---
+++ myScripts.js
@@ -0,0 +1,8 @@
+document.getElementById("h2").innerHTML = "Jannat Onalar Oyogi Ostidadur";
+	document.getElementById("boyaka").innerHTML = "Assalom alekum";
+
+	function myFunction() {
+		document.getElementById("idp").style.fontSize = "30px";
+		document.getElementById("idp").style.color = "red";
+		document.getElementById("idp").style.backgroundColor = "blue"; 
+	}
